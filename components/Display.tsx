4ada32a53ac43f1f074ff58dbbb9970b95ab9863
chanki <chanki.kim89@gmail.com>
2023-04-28 16:30:51 +0900

    webview설치

--- components/Display.tsx
+++ components/Display.tsx
@@ -1,29 +1,54 @@
-import React from 'react';
-import {
-  SafeAreaView, ScrollView, StyleSheet, Text, 
-} from 'react-native';
+import React, {useRef} from 'react';
+import {StyleSheet, Alert, BackHandler} from 'react-native';
+import WebView from 'react-native-webview';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../types';
+export type DisplayScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'Display'
+>;
 
+export default function Display({navigation, route}: DisplayScreenProps) {
+  const WEBVIEW_REF = useRef<WebView | null>(null);
+  const handleError = () =>
+    Alert.alert('네트워크 실패', '인터넷 연결을 확인해주세요', [
+      {
+        text: '재시도',
+        style: 'cancel',
+        onPress: () => {
+          WEBVIEW_REF && WEBVIEW_REF.current && WEBVIEW_REF.current.reload();
+        },
+      },
+      {
+        text: '뒤로가기',
+        onPress: () => {
+          navigation.goBack();
+        },
+      },
+    ]);
 
-export default function Home(){
-    return <SafeAreaView>
-    <ScrollView contentContainerStyle={styles.contaienr}>
-      <Text> MENU DISPLAY</Text>
-    </ScrollView>
-  </SafeAreaView>
+  return (
+    <WebView
+      ref={WEBVIEW_REF}
+      source={{uri: 'http://heavybear.cafe24.com/did/'}}
+      style={{alignSelf: 'stretch'}}
+      onError={handleError}
+      onHttpError={handleError}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    contaienr : {
-      height:'100%',
-      backgroundColor: '#02072D',
-      alignItems:'center',
-      justifyContent:'center'
-    },
-  
-    buttonContainer : {
-      flexDirection:'row',
-      marginTop:30,
-      gap:10
-    }
-  })
-  
+  contaienr: {
+    height: '100%',
+    backgroundColor: '#02072D',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 30,
+    gap: 10,
+  },
+});
